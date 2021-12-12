cd2c57416b96c86dcd184b48de46eb911d09d236
pilarxyz <84503940+pilarxyz@users.noreply.github.com>
2021-12-12 21:37:39 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Database Biodiversity Indonesia</title>
+    <link rel="stylesheet" type="text/css" href="style.css" media="all" />
+  </head>
+<body>
+  <div class="menu-wrap">
+  <ul>
+    <li><a href="index.php">Beranda</a></li>
+    <li><a href="data.php">Database</a>
+      <ul>
+        <li><a href="flora.php">Flora</a></li>
+        <li><a href="fauna.php">Fauna</a></li>
+      </ul>
+    </li>
+    <li><a href="search.php">Pencarian</a></li>
+    <li><a href="tambahdata.php">Tambah Data</a></li>
+  </ul>
+  </div>
+</body>
+<head>
+        <!-- Load file CSS Bootstrap offline -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+</head>
+<body>
+<div class="container">
+    <br>
+    <h4>Database Biodiversity Indonesia</h4>
+<br>
+    <h5> Anggota Kelompok </h5>
+    <h6> - Nina <br> - Pilar <br> - Nita <br> - Tita </h6>
+
+    <?php
+      $queries = array();
+      parse_str($_SERVER['QUERY_STRING'], $queries);
+      error_reporting(E_ALL ^ (E_NOTICE | E_WARNING));
+      switch ($queries['page']) {
+      	case 'tambah_data':
+      		# code...
+      		include 'tambahdata.php';
+      		break;
+
+              case 'data':
+                # code...
+                include 'data.php';
+                break;
+
+        default:
+		          #code...
+		      include 'none.php';
+		      break;
+        }
+        ?>
+
+</html>
